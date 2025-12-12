4a796be40df178513d7610bf01c56f5a74a83a83
CristianoSP <cristianos.pontes@hotmail.com>
2025-12-12 11:16:15 -0400

    ajjuste

--- forms/src/pages/forms/InFile.jsx
+++ forms/src/pages/forms/InFile.jsx
@@ -1,17 +1,19 @@
+import 'react'
 import './style.css'
 import './script.js'
+import '../../App.css'
 
 function InFile() {
     return (
-<div>
-    <div class="file-input-container">
-        <input type="file" id="fileInput" class="file-input">
-        <label for="fileInput" class="file-label">
-        <svg class="upload-icon" viewBox="0 0 24 24">
+<div className='container'>
+    <div className="file-input-container">
+        <input type="file" id="fileInput" className="file-input">
+        <label for="fileInput" className="file-label">
+        <svg className="upload-icon" viewBox="0 0 24 24">
         <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
         </svg>
-        <span class="file-text">Escolher arquivo</span>
-        <span class="file-name">Nenhum arquivo selecionado</span>
+        <span className="file-text">Escolher arquivo</span>
+        <span className="file-name">Nenhum arquivo selecionado</span>
         </label>
         </input>
     </div>
